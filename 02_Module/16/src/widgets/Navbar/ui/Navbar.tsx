b67e8dd1159c37d819ea611c9c70a51b6af62922
chewmountain <mironov.develop@gmail.com>
2023-06-21 16:10:18 +0300

    2023-06-21 Since v4.0.0 Webpack Hot Module Replacement is enabled by default

---
+++ 02_Module/16/src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,30 @@
+import styles from "./Navbar.module.scss";
+import { classNames } from "shared/lib/classNames/classNames";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { ThemeSwitcher } from "widgets/ThemeSwitcher";
+
+interface NavbarProps {
+    className?: string;
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+    return (
+        <div className={classNames(styles.Navbar, {}, [className])}>
+            <div className={styles.links}>
+                {/**
+                 * Отдельно вынесли ссылки с роутами
+                 */}
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to={"/"}
+                    className={styles.mainLink}
+                >
+                    Главная страница
+                </AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>
+                    О нас
+                </AppLink>
+            </div>
+        </div>
+    );
+};
---
+++ 02_Module/17/src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -0,0 +1,25 @@
+import styles from "./ThemeSwitcher.module.scss";
+import { classNames } from "shared/lib/classNames/classNames";
+import { Theme, useTheme } from "app/providers/ThemeProvider";
+import LightIcon from "shared/assets/icons/theme-light.svg";
+import DarkIcon from "shared/assets/icons/theme-dark.svg";
+import { Button } from "widgets/Navbar/ui/button";
+import { ThemeButton } from "widgets/Navbar/ui/button/ui/Button";
+
+interface ThemeSwitcherProps {
+    className?: string;
+}
+
+export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
+    const { theme, toggleTheme } = useTheme();
+
+    return (
+        <Button
+            theme={ThemeButton.CLEAR}
+            onClick={toggleTheme}
+            className={classNames(styles.ThemeSwitcher, {}, [className])}
+        >
+            {theme === Theme.LIGHT ? <LightIcon /> : <DarkIcon />}
+        </Button>
+    );
+};
