49ae2c661daf1ef6a35def289420bf4fe7e1e6a1
chewmountain <mironov.develop@gmail.com>
2023-06-22 13:45:40 +0300

    2023-06-22 i18n Интернационализация приложения. Объявление глобальное переменной define plugin webpack

--- 02_Module/15/src/shared/config/i18n/i18n.ts
+++ 02_Module/15/src/shared/config/i18n/i18n.ts
@@ -2,7 +2,10 @@ import i18n from "i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-// https://www.i18next.com/
+/**
+ * https://www.i18next.com/
+ * Интернационализация приложения
+ */
 i18n.use(Backend)
     .use(LanguageDetector)
     .use(initReactI18next)
